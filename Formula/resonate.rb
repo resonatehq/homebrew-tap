b05356d5203b1c8bec1b3596ebe7c13b9a7239e7
dfarr <dfarr@users.noreply.github.com>
2025-11-18 17:19:00 +0000

    Bump version to 0.7.24

--- Formula/resonate.rb
+++ Formula/resonate.rb
@@ -1,5 +1,5 @@
 class Resonate < Formula
-  version '0.7.23'
+  version '0.7.24'
   desc "A dead simple programming model for the cloud"
   homepage "https://github.com/resonatehq/resonate"
   license "Apache-2.0"
@@ -8,18 +8,18 @@ class Resonate < Formula
   if OS.mac?
       if Hardware::CPU.arm?
           url "https://github.com/resonatehq/resonate/releases/download/v#{version}/resonate_darwin_aarch64.tar.gz"
-          sha256 "40841df3d49383c3af44f641f053c5b557c9c68845b463d5c7bb25e3d9fdffe6"
+          sha256 "707e03b87e3bbdaf9b859564b4db3f331ed773e365e3b7f1531cca755f30db2f"
       else
           url "https://github.com/resonatehq/resonate/releases/download/v#{version}/resonate_darwin_x86_64.tar.gz"
-          sha256 "53ca135d3acb5d8983d75f6ebf2d30ff109ae79cbf2ea92967d4fcb11485b494"
+          sha256 "13a763ffca61cb49511d89352f99ae451fc5d9d91aab8c212451ed59de1c430d"
       end
   elsif OS.linux?
      if Hardware::CPU.arm?
          url "https://github.com/resonatehq/resonate/releases/download/v#{version}/resonate_linux_aarch64.tar.gz"
-         sha256 "5c1b4e159bf48318851dc6d68daf35587a84b84bca4cc74c02d4ca3fd172f677"
+         sha256 "1d2d122877c55274751f953ef87d1600bb33e87ab4a0736c44200aedca7663cf"
      else
          url "https://github.com/resonatehq/resonate/releases/download/v#{version}/resonate_linux_x86_64.tar.gz"
-         sha256 "4f975397496c19a345f8963df7391cff2b379cbfd860990c3076c5bfffacb78a"
+         sha256 "cded03b585139bbc1d8b2fc3879ef05b5a30a9bc8f9488c18d368cf1423d24a7"
      end
   end
 
