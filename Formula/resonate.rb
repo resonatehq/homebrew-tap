6ad3fe9a72b35d3b6f2e5b19785d792bd2d3c233
David Farr <david@resonatehq.io>
2026-05-03 15:37:49 -0700

    chore: update resonate to v0.9.6 (#24)
    Automated update from resonatehq/resonate release workflow.
    
    - Version: v0.9.6
    - Release: https://github.com/resonatehq/resonate/releases/tag/v0.9.6
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/resonate.rb
+++ Formula/resonate.rb
@@ -1,23 +1,23 @@
 class Resonate < Formula
-  version '0.9.5'
+  version '0.9.6'
   desc "A dead simple programming model for the cloud"
   homepage "https://github.com/resonatehq/resonate"
   arch = Hardware::CPU.arch.to_s
   if OS.mac?
       if Hardware::CPU.arm?
           url "https://github.com/resonatehq/resonate/releases/download/v#{version}/resonate_darwin_aarch64.tar.gz"
-          sha256 "1b573a8388e7e16fa88437f989667af317e94b1b70404ebc971bb49afe0b74ad"
+          sha256 "c7d8c579a59e5b05e113c281eb790ae45ac3dd094993c62a5fbb414e05e93c90"
       else
           url "https://github.com/resonatehq/resonate/releases/download/v#{version}/resonate_darwin_x86_64.tar.gz"
-          sha256 "879f3409257b8c26a74237edf8d8c5cf7d9468246e7bce584438bf2921f31704"
+          sha256 "8b8206ccdb4c4ac42d5b25d822028b864791906b6823ec30eaefcc9d3f6acba6"
       end
   elsif OS.linux?
      if Hardware::CPU.arm?
          url "https://github.com/resonatehq/resonate/releases/download/v#{version}/resonate_linux_aarch64.tar.gz"
-         sha256 "257e3e6040157288894fc47381c0f5a3016d44ca20e6ded8f8b354b5fcc7add9"
+         sha256 "4c7e11364ebab6300462441dad734587616df814dbc1f59d364052d18a7ae6aa"
      else
          url "https://github.com/resonatehq/resonate/releases/download/v#{version}/resonate_linux_x86_64.tar.gz"
-         sha256 "7760ea0baa163324e348ed6c94cf867e4f67e6469d1b5982c000eed8040fc4e2"
+         sha256 "0c4aa1761fdd35255b5c5057db1e9091ea18a51d5a680156f7178db25d560088"
      end
   end
 
